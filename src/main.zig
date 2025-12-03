0132bbf3ef7f237d61d9e8a00f61aa1fd77aa0e0
Kutay Yilmaz <ktyylmz035@gmail.com>
2025-12-03 23:21:05 +0300

    gui

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,36 @@
-// const webui = @import("webui");
+const webui = @import("webui");
+const std = @import("std");
 const zalgebra = @import("zalgebra");
 
-pub fn main() !void {
-    // create a new window
-    // var nwin = webui.newWindow();
+const html_file = @embedFile("gui/html/index.html");
 
-    // // show the content
+fn close_window(e: *webui.Event) void {
+    const win = e.getWindow();
+    win.close();
+}
+
+fn always_true(_: usize) bool {
+    return true;
+}
+
+pub fn main() !void {
+    var nwin = webui.newWindow();
+
+    nwin.setCloseHandlerWv(always_true);
+
+    _ = try nwin.bind("close_window", close_window);
+
+    nwin.setSize(1920, 1080);
+    nwin.setPosition((3840 - 1920) / 2, (2160 - 1080) / 2);
+    nwin.setFrameless(true);
+    // nwin.setTransparent(true);
+    nwin.setResizable(true);
+
+    // show the content
     // nwin.setFrameless(true);
-    // nwin.setCenter();
-    // try nwin.show("<html><head><script src=\"/webui.js\"></script></head> Hello World ! </html>");
-    // // wait the window exit
-    // webui.wait();
+    try nwin.showWv(html_file);
+
+    webui.wait();
+
+    // webui.clean();
 }
