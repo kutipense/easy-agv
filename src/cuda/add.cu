f54dc333d37eb7dbee6b177fc55b291b118b50d6
Kutay Yilmaz <ktyylmz035@gmail.com>
2025-12-04 01:50:30 +0300

    add cuda /w gemini 3

---
+++ src/cuda/add.cu
@@ -0,0 +1,9 @@
+extern "C" __global__ void addKernel(int *a, int *b, int *c)
+{
+    *c = *a + *b;
+}
+
+extern "C" void launchAddKernel(int *a, int *b, int *c)
+{
+    addKernel<<<1, 1>>>(a, b, c);
+}
--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,43 @@ fn always_true(_: usize) bool {
     return true;
 }
 
+extern fn cudaMalloc(ptr: *?*anyopaque, size: usize) c_int;
+extern fn cudaMemcpy(dest: *anyopaque, src: *const anyopaque, size: usize, kind: c_int) c_int;
+extern fn cudaFree(ptr: *anyopaque) c_int;
+extern "C" fn launchAddKernel(a: *anyopaque, b: *anyopaque, c: *anyopaque) void;
+
+const cudaMemcpyHostToDevice = 1;
+const cudaMemcpyDeviceToHost = 2;
+
+export fn add(a: i32, b: i32) i32 {
+    var d_a: ?*anyopaque = undefined;
+    var d_b: ?*anyopaque = undefined;
+    var d_c: ?*anyopaque = undefined;
+    var result: i32 = undefined;
+
+    _ = cudaMalloc(&d_a, @sizeOf(i32));
+    _ = cudaMalloc(&d_b, @sizeOf(i32));
+    _ = cudaMalloc(&d_c, @sizeOf(i32));
+
+    _ = cudaMemcpy(d_a.?, &a, @sizeOf(i32), cudaMemcpyHostToDevice);
+    _ = cudaMemcpy(d_b.?, &b, @sizeOf(i32), cudaMemcpyHostToDevice);
+
+    launchAddKernel(d_a.?, d_b.?, d_c.?);
+
+    _ = cudaMemcpy(&result, d_c.?, @sizeOf(i32), cudaMemcpyDeviceToHost);
+
+    _ = cudaFree(d_a.?);
+    _ = cudaFree(d_b.?);
+    _ = cudaFree(d_c.?);
+
+    return result;
+}
+
 pub fn main() !void {
+    const result = add(12, 12);
+
+    std.debug.print("result: {d}\n\n", .{result});
+
     var nwin = webui.newWindow();
 
     nwin.setCloseHandlerWv(always_true);
