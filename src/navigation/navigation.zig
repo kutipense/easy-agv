a99b98eb63854bc6062c9b55fc83c5fbb0da92e4
Kutay Yilmaz <ktyylmz035@gmail.com>
2025-12-08 01:04:29 +0300

    sync

--- src/navigation/navigation.zig
+++ src/navigation/navigation.zig
@@ -8,14 +8,14 @@ const LocalPlanner = @import("local_planner.zig").LocalPlanner;
 const GlobalPlanner = @import("global_planner.zig").GlobalPlanner;
 const Costmap = @import("costmap.zig").Costmap;
 
+const Rate = @import("../utils/rate.zig").Rate;
+
 const Pose = geometry_types.Pose;
 const Map2D = map_types.Map2D;
 const Plan = navigation_types.Plan;
 
 const Navigation = struct {
     path_update_tolerance: u32 = 1,
-
-    stopped: bool = false,
 
     controller: anyopaque,
 
@@ -29,20 +29,59 @@ const Navigation = struct {
     latest_plan: ?Plan = null,
     global_plan_mtx: std.Thread.Mutex = .{},
 
+    target: ?Pose,
+
+    global_plan_loop_mtx: std.Thread.Mutex,
+    global_plan_loop_cond: std.Thread.Condition,
+    stopped: std.atomic.Value(bool),
+    aborted: std.atomic.Value(bool),
+
     pub fn init(self: *Navigation) !void {
+        self.stopped = std.atomic.Value(bool).init(false);
+        self.aborted = std.atomic.Value(bool).init(false);
         self.global_thread = try std.Thread.spawn(.{}, self.global_loop, .{self}) catch unreachable;
     }
 
     pub fn deinit(self: *Navigation) void {
+        self.stopped.store(true, .release);
+        self.aborted.store(true, .release);
+        self.global_plan_loop_cond.signal();
         self.global_thread.join();
     }
 
     fn global_loop(self: *Navigation) void {
-        while (!self.stopped) {
-            self.global_plan_mtx.lock();
-            self.latest_plan = null;
-            self.global_plan_mtx.unlock();
+        while (!self.stopped.load(.acquire)) {
+            self.global_plan_loop_mtx.lock();
+            while (self.target == null and !self.stopped.load(.acquire)) {
+                self.global_plan_loop_cond.wait(self.global_plan_loop_mtx);
+            }
+            const target = self.target.?;
+            self.target = null;
+            self.aborted.store(false, .release);
+            self.global_plan_loop_mtx.unlock();
+
+            var rate = try Rate.init(1);
+            while (!self.stopped.load(.acquire) and !self.aborted.load(.acquire)) {
+                if (self.costmap.global_costmap()) |costmap| {
+                    const pose = self.localization.get_pose();
+                    if (self.global_planner.get_path(costmap, pose, target)) |plan| {
+                        self.global_plan_mtx.lock();
+                        self.latest_plan = plan;
+                        self.global_plan_mtx.unlock();
+                    }
+                }
+                if (!self.aborted.load(.acquire))
+                    rate.sleep();
+            }
         }
+    }
+
+    fn set_target(self: *Navigation, target: Pose) void {
+        self.aborted.store(true, .release);
+        self.global_plan_loop_mtx.lock();
+        self.target = target;
+        self.global_plan_loop_cond.signal();
+        self.global_plan_loop_mtx.unlock();
     }
 
     fn global_planner_step(self: *Navigation, goal: Pose) void {
